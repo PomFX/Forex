bac849548d6f2e471ab27545f7bcefb4f0a7f240
PomFX <pjncomputer@gmail.com>
2026-05-24 17:38:15 +0700

    Increase JSON body limit to 50mb for QR uploads

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const { initDB } = require('../server/db');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/auth', require('../server/routes/auth'));
 app.use('/api/signals', require('../server/routes/signals'));
